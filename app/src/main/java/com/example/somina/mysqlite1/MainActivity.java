fe1b5326d04abca9db640f1f622186c653493fb4
Subhrajyoti123 <subhra.brahma9@gmail.com>
2017-02-23 20:12:58 +0530

    SQLite
    assignment7.2

---
+++ app/src/main/java/com/example/somina/mysqlite1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.somina.mysqlite1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView autoCompleteTextView;
+    TextView textView;
+    ArrayList <String>productname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textview);
+
+        productname =new ArrayList<>();
+        productname.add("Hp Inkjet Printer");
+        productname.add("Himalaya Shampoo");
+        productname.add("Google Pixel");
+        productname.add("Godrej Washing Machine");
+        productname.add("Apple Macbook Pro");
+        productname.add("Amway Lipstick");
+
+        DataBase dataBase = new DataBase(this);
+
+        for(int i = 0;i<productname.size();i++){
+            dataBase.addProducts(productname.get(i));
+        }
+
+
+
+        ArrayList arrayList = dataBase.Databasetoarray();
+
+
+
+
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autocompletetextview);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.select_dialog_item,arrayList);
+        autoCompleteTextView.setThreshold(1);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+
+
+
+    }
+}
